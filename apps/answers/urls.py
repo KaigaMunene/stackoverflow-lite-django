83f625703922b1bea38b1c5546ce4070180693bd
KaigaMunene <kaigamunene@gmail.com>
2022-03-21 11:53:51 +0300

    feature(project): answers app
    * create views for get, put and delete methods
    * create the corresponding urls for the views

--- apps/answers/urls.py
+++ apps/answers/urls.py
@@ -3,5 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("answer/<int:question>", views.post_answer, name="post_an_answer")
+    path(
+        "answer/post_and_get/<int:question>",
+        views.post_answer,
+        name="post_an_answer",
+    ),
+    path("answers/", views.get_all_answers, name="get_all_answers"),
+    path(
+        "answer/update/<int:question>",
+        views.update_an_answer,
+        name="update_answer",
+    ),
+    path("answer/delete/<int:id>", views.delete_answer, name="delete_answer"),
 ]
